5216c28c3bef7d4a5b56050dfedbf05b3951abf3
MuhammadSaadSheikh <msaad9124@gmail.com>
2021-11-10 16:32:24 +0000

    google map inprogress

--- App.js
+++ App.js
@@ -1,9 +1,80 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, Text, Button, Permission, PermissionStatus, PermissionsAndroid } from 'react-native'
+import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const App = () => {
-  return(
-    <View style={{flex: 1}}></View>
+
+  //podfile ios
+  // pod 'react-native-maps', :path => '../node_modules/react-native-maps'
+
+  //application java
+  // import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
+  // import com.airbnb.android.react.maps.MapsPackage;
+  // import com.airbnb.android.react.maps.MapsPackage;
+
+  //setting gradlw
+  // include ':react-native-android-location-services-dialog-box'
+  // project(':react-native-android-location-services-dialog-box').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-android-location-services-dialog-box/android')
+  // include ':react-native-maps'
+  // project(':react-native-maps').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-maps/lib/android')
+  // include ':react-native-maps'
+  // project(':react-native-maps').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-maps/lib/android')
+
+  //package-lockjson
+
+
+  useEffect(() => {
+    // granted()
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log('position', position);
+      },
+      (error) => {
+        // See error code charts below.
+        console.log('position error', error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, [permisson])
+
+  const permisson = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Cool Photo App Camera Permission",
+          message:
+            "Cool Photo App needs access to your camera " +
+            "so you can take awesome pictures.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      {/* <Button title='Granted' onPress={granted}/> */}
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
   )
 }
 
